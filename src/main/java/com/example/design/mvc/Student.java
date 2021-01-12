604e172bf8416998de5edf58b9b4a82bccdfb11f
Arte <jiangtao.cheng@orderplus.com>
2021-01-12 09:53:09 +0800

    mvc pattern

---
+++ src/main/java/com/example/design/mvc/Student.java
@@ -0,0 +1,26 @@
+package com.example.design.mvc;
+
+/**
+ * @author Arte
+ * @create 21-1-12
+ */
+public class Student {
+    private String rollNo;
+    private String name;
+
+    public String getRollNo() {
+        return rollNo;
+    }
+
+    public void setRollNo(String rollNo) {
+        this.rollNo = rollNo;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/com/example/design/mvc/StudentView.java
@@ -0,0 +1,14 @@
+package com.example.design.mvc;
+
+/**
+ * @author Arte
+ * @create 21-1-12
+ */
+public class StudentView {
+
+    public void printStudentDetails(String studentName, String studentRollNo) {
+        System.out.println("Student: ");
+        System.out.println("Name: " + studentName);
+        System.out.println("Roll No: " + studentRollNo);
+    }
+}
