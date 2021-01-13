a33cfd0831663cce08d120a2f9d7d23076240278
Arte <jiangtao.cheng@orderplus.com>
2021-01-13 09:48:01 +0800

    composite entity pattern

---
+++ src/main/java/com/example/design/compositeentity/CompositeEntity.java
@@ -0,0 +1,17 @@
+package com.example.design.compositeentity;
+
+/**
+ * @author Arte
+ * @create 21-1-13
+ */
+public class CompositeEntity {
+    private CoarseGrainedObject cgo = new CoarseGrainedObject();
+
+    public void setData(String data1, String data2) {
+        cgo.setData(data1, data2);
+    }
+
+    public String[] getData() {
+        return cgo.getData();
+    }
+}
---
+++ src/main/java/com/example/design/compositeentity/DependentObject1.java
@@ -0,0 +1,18 @@
+package com.example.design.compositeentity;
+
+/**
+ * @author Arte
+ * @create 21-1-13
+ */
+public class DependentObject1 {
+
+    private String data;
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+}
